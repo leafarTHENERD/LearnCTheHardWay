f1cb91c8b8f9cdff58abdb6600dec6e56001100d
LeafarTHENERD <leafar.thenerd@hotmail.com>
2015-07-16 22:09:17 -0300

    Final alignment

---
+++ ex2/ex2.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	puts("Hello world.");
+	puts("More that a 'hello world'.");
+	puts("Trying some arguments (%d, %s).");
+	puts("Just for completing 5 lines.");
+	puts("End. :-)");
+
+	return 0;
+}
